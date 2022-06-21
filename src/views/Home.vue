e4c4ca3631aa5b8963511240ffce859622fc0e94
vollcheck <42350899+vollcheck@users.noreply.github.com>
2022-06-21 20:38:24 +0200

    commit changes from login+register+profilview

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <header-component />
+  <h2>Home</h2>
+</template>
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue';
+export default {
+  components: { HeaderComponent },
+  name: "Home",
+}
+</script>
---
+++ src/views/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+    <header-component />
+    <div>
+        <h2>Hi</h2>
+    </div>
+</template>
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue';
+import store from '../store/index.js'
+export default {
+
+    components: { HeaderComponent },
+    name: 'Profile',
+    computed: {
+        currentUser() {
+            return store.state.loggedIn;
+        }
+    },
+};
+</script>
